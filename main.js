aa46dbfff1e91dca338576006129014c992f997c
sjunlee0116 <sjunlee0116@gmail.com>
2026-03-17 13:04:41 +0900

    v1.0

---
+++ main.js
@@ -0,0 +1,17 @@
+document.getElementById('time').innerHTML = new Date().toLocaleString()
+
+function pink() {
+  document.body.style.backgroundColor = 'pink'
+}
+function lightblue() {
+    document.body.style.backgroundColor = 'lightblue'
+}
+function  yellow() {
+    document.body.style.backgroundColor = 'yellow'
+}
+function reset() {
+    document.body.style.backgroundColor = 'linen'
+}
+
+ㅊ
+
