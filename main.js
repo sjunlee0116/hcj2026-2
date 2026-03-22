136abe6383262e0113b918173642b1e9ec50b9a2
sjunlee0116 <sjunlee0116@gmail.com>
2026-03-23 00:25:27 +0900

    프로젝트 파일 업데이트

--- main.js
+++ main.js
@@ -4,14 +4,31 @@ function pink() {
   document.body.style.backgroundColor = 'pink'
 }
 function lightblue() {
-    document.body.style.backgroundColor = 'lightblue'
+  document.body.style.backgroundColor = 'lightblue'
 }
-function  yellow() {
-    document.body.style.backgroundColor = 'yellow'
+function yellow() {
+  document.body.style.backgroundColor = 'yellow'
 }
 function reset() {
-    document.body.style.backgroundColor = 'linen'
+  document.body.style.backgroundColor = 'linen'
 }
 
-ㅊ
-
+function showhtml() {
+  document.getElementById('fig').src = 'img/html5.png'
+  document.getElementById('desc').innerHTML =
+    '<h3>HTML</h3>은 구조적 웹문서를 작성하는데 사용하는 마크업 언어이다'
+}
+function showcss() {
+  document.getElementById('fig').src = 'img/css3.png'
+  document.getElementById('desc').innerHTML =
+    '<span style="color:red;">CSS3</span>는 HTML 문서의 스타일을 지정하기 위한 언어이다.'
+}
+function showjs() {
+  document.getElementById('fig').src = 'img/js.png'
+  document.getElementById('desc').innerHTML =
+    'Javascript는 웹을 위한 프로그래밍 언어이며 웹문서에서 동작을 실행할 수 있게 한다.'
+}
+function hide() {
+  document.getElementById('fig').src = ''
+  document.getElementById('desc').innerHTML = ''
+}
